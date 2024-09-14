49a0febcd9ce0ea3b14826d3898e346ed8d9b19a
liyuxiang1111 <739559572@qq.com>
2024-09-14 09:51:56 +0800

    2024/9/14 不需要的文件删除

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,9 +6,7 @@ import org.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 @MapperScan("com.example.demo.Mapper")  // 确保这里的包名与 Mapper 接口所在的包一致
 public class DemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
 }
